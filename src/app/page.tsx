928b1ca53b9c8441bd8135bc9e9d1696cc958977
Sahil <sk2003311@gmail.com>
2024-09-07 15:24:34 +0530

    land page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
-
+import CustomButton from "@/components/CustomButton";
 
 export default function Home() {
- return(
-  <></>
- )
+  return (
+    <div className="w-screen h-screen flex flex-col justify-center items-center gap-[1rem]">
+      <p className="text-white">website in progress, in meantime play</p>
+      <a href="/auth"><CustomButton label={"Login"} /></a>
+      
+    </div>
+  );
 }
